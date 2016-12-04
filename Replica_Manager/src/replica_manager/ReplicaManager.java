3d69e4f9e1a7ae99aa62c1304df9cb8f71108630
Jeremy Melnyk <jeremyjm91@gmail.com>
2016-12-04 11:46:56 -0500

    Moving over changes from reboot_algorithm. Executing remote process.

--- Replica_Manager/src/replica_manager/ReplicaManager.java
+++ Replica_Manager/src/replica_manager/ReplicaManager.java
@@ -11,7 +11,12 @@ public class ReplicaManager implements Runnable {
 	private final int THREAD_POOL_SIZE = Integer.MAX_VALUE;
 	private final ExecutorService threadPool;
 	int port;
-
+	
+	public static void main(String[] args){
+		ReplicaManager r = new ReplicaManager(5000);
+		r.initReplicas(args);
+	}
+	
 	public ReplicaManager(int port) {
 		super();
 		this.threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
@@ -21,6 +26,23 @@ public class ReplicaManager implements Runnable {
 	@Override
 	public void run() {
 		serveRequests();
+	}
+	
+	private void initReplicas(String[] args){
+		String jeremyPath = "java -classpath ..\\Jeremy_Replica\\bin server.PublishingServer";
+		Process p = null;
+		try {
+			p = Runtime.getRuntime().exec(jeremyPath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Testing process kill
+		if(p != null)
+		{
+			p.destroy();	
+		}
 	}
 
 	private void serveRequests() {
