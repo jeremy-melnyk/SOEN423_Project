3e0bb418173050a17d82f981eb6caa6dec048841
Jeremy Melnyk <jeremyjm91@gmail.com>
2016-12-05 15:28:43 -0500

    Created client for FE and models. FE placeholder for getFlights and getReservations. Tested client with FE, formatting is correct.

---
+++ CORBA_DFRS/src/client/CorbaClient.java
@@ -0,0 +1,104 @@
+package client;
+
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+
+import enums.City;
+import friendly_end.FlightReservationServer;
+import friendly_end.FlightReservationServerHelper;
+
+public class CorbaClient {
+	protected final String NAME_SERVICE = "NameService";
+	protected final String FRONT_END_NAME = "DFRS";
+	protected City city;
+	protected String lastName;
+	protected String firstName;
+	protected ORB orb;
+	
+	public CorbaClient(City city, String lastName, String firstName, ORB orb) {
+		super();
+		this.city = city;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.orb = orb;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public ORB getOrb() {
+		return orb;
+	}
+
+	public void setOrb(ORB orb) {
+		this.orb = orb;
+	}
+	
+	protected String[] getFlightRecords(){
+		FlightReservationServer flightServer = getFlightServer();
+		if (flightServer == null){
+			System.out.println("FlightServer was null for " + city);
+			return new String[0];
+		}
+		return flightServer.getFlights();
+	}
+	
+	protected String[] getFlightReservations(){
+		FlightReservationServer flightServer = getFlightServer();
+		if (flightServer == null){
+			System.out.println("FlightServer was null for " + city);
+			return new String[0];
+		}
+		return flightServer.getReservations();
+	}
+
+	// TODO : Replace with CORBA lookup
+	protected FlightReservationServer getFlightServer(){
+		org.omg.CORBA.Object nameServiceRef;
+		try {
+			nameServiceRef = orb.resolve_initial_references(NAME_SERVICE);
+			NamingContextExt namingContextRef = NamingContextExtHelper.narrow(nameServiceRef);
+			org.omg.CORBA.Object flightReservationServerRef = namingContextRef.resolve_str(FRONT_END_NAME);
+			FlightReservationServer flightReservationServer = FlightReservationServerHelper.narrow(flightReservationServerRef);
+			return flightReservationServer;
+		} catch (InvalidName e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotFound e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotProceed e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
