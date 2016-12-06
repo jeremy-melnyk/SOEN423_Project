2642bad7a43c860fd636948f7a56307b528dd58f
Jeremy Melnyk <jeremyjm91@gmail.com>
2016-12-06 13:02:11 -0500

    ignore case

--- CORBA_DFRS/src/packet/EditFlightRecordReply.java
+++ CORBA_DFRS/src/packet/EditFlightRecordReply.java
@@ -50,42 +50,42 @@ public class EditFlightRecordReply extends OperationParameters {
 		if (businessSeats == null) {
 			if (other.businessSeats != null)
 				return false;
-		} else if (!businessSeats.equals(other.businessSeats))
+		} else if (!businessSeats.equalsIgnoreCase(other.businessSeats))
 			return false;
 		if (date == null) {
 			if (other.date != null)
 				return false;
-		} else if (!date.equals(other.date))
+		} else if (!date.equalsIgnoreCase(other.date))
 			return false;
 		if (destination == null) {
 			if (other.destination != null)
 				return false;
-		} else if (!destination.equals(other.destination))
+		} else if (!destination.equalsIgnoreCase(other.destination))
 			return false;
 		if (economySeats == null) {
 			if (other.economySeats != null)
 				return false;
-		} else if (!economySeats.equals(other.economySeats))
+		} else if (!economySeats.equalsIgnoreCase(other.economySeats))
 			return false;
 		if (error == null) {
 			if (other.error != null)
 				return false;
-		} else if (!error.equals(other.error))
+		} else if (!error.equalsIgnoreCase(other.error))
 			return false;
 		if (firstSeats == null) {
 			if (other.firstSeats != null)
 				return false;
-		} else if (!firstSeats.equals(other.firstSeats))
+		} else if (!firstSeats.equalsIgnoreCase(other.firstSeats))
 			return false;
 		if (flightId == null) {
 			if (other.flightId != null)
 				return false;
-		} else if (!flightId.equals(other.flightId))
+		} else if (!flightId.equalsIgnoreCase(other.flightId))
 			return false;
 		if (origin == null) {
 			if (other.origin != null)
 				return false;
-		} else if (!origin.equals(other.origin))
+		} else if (!origin.equalsIgnoreCase(other.origin))
 			return false;
 		return true;
 	}
@@ -96,7 +96,7 @@ public class EditFlightRecordReply extends OperationParameters {
 			return error;
 		}
 		StringBuilder sb = new StringBuilder();
-		sb.append(flightId + Constants.DELIMITER + origin + Constants.DELIMITER + destination + Constants.DELIMITER + date);
+		sb.append(flightId + Constants.DELIMITER + origin.toUpperCase() + Constants.DELIMITER + destination.toUpperCase() + Constants.DELIMITER + date);
 		sb.append(Constants.DELIMITER + "E" + economySeats);
 		sb.append(Constants.DELIMITER + "B" + businessSeats);
 		sb.append(Constants.DELIMITER + "F" + firstSeats);
